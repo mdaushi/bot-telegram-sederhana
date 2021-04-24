9afa53f4de1cda02374875b49d6f07f84639e1ba
muhfirdaus19 <firdaus.ex19@gmail.com>
2021-04-25 03:33:30 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// const { Telegraf } = require('telegraf')
-const { Composer } = require('micro-bot')
-const bot = new Composer
-// const bot = new Telegraf('1626615561:AAHExRvQ-YmYJJ8EjJonBH4qtlgShVbLl-0')
+const config = require('./config')
+if(config.status == 'prod'){
+    const { Composer } = require('micro-bot')
+    var bot = new Composer
+}else{
+    const { Telegraf } = require('telegraf')
+    var bot = new Telegraf('1626615561:AAHExRvQ-YmYJJ8EjJonBH4qtlgShVbLl-0')
+}
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-// bot.launch()
-module.exports = bot
+if(config.status == 'prod'){
+    module.exports = bot
+}else{
+    bot.launch()
+}
 
 
 // Enable graceful stop
