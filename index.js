50a67d9ca5c57a74c9826caa460d2a518b89418a
muhfirdaus19 <firdaus.ex19@gmail.com>
2021-04-27 17:20:25 +0800

    webhook

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// const config = require('./config')
-// if(config.status == 'prod'){
-//     const { Composer } = require('micro-bot')
-//     var bot = new Composer
-// }else{
-//     const { Telegraf } = require('telegraf')
-//     var bot = new Telegraf('1626615561:AAHExRvQ-YmYJJ8EjJonBH4qtlgShVbLl-0')
-// }
-// require('./help')(bot)
-// if(config.status == 'prod'){
-//     module.exports = bot
-// }else{
-//     bot.launch()
-// }
-
-
-// // Enable graceful stop
-// process.once('SIGINT', () => bot.stop('SIGINT'))
-// process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf('1626615561:AAHExRvQ-YmYJJ8EjJonBH4qtlgShVbLl-0')
